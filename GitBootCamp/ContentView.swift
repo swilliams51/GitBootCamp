ee46d20375cc575b334e4e1f33263a872f225099
steve williams <sjwilliams51@gmail.com>
2024-08-10 11:59:28 -0400

    Initial Commit

---
+++ GitBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitBootCamp
+//
+//  Created by Steven Williams on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitBootCamp/GitBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBootCampApp.swift
+//  GitBootCamp
+//
+//  Created by Steven Williams on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
